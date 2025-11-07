04a8029a75ab43425be14be1b834dded0d35364a
Ethan P <ethan.j.pope09@gmail.com>
2025-11-06 20:14:04 -0700

    Adding my window scripts and made my game changer parts so it seamlessly switches from different windows

--- EthansNYTGames/Assets/Scripts/Window.cs
+++ EthansNYTGames/Assets/Scripts/Window.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-public class Window : MonoBehaviour
+public class Window : MonoBehaviour, IWindow
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Show()
     {
-        
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
-        
+        gameObject.SetActive(false);
     }
 }
--- EthansNYTGames/Assets/Scripts/WindowManager.cs
+++ EthansNYTGames/Assets/Scripts/WindowManager.cs
@@ -1,16 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WindowManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static WindowManager Instance;
+
+    [SerializeField] private List<Window> windows;
+    [SerializeField] private Window startingWindow;
+    private Window currentWindow;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentWindow = startingWindow;
+    }
+
+    public void OpenWindow(Window window)
+    {
+        if (currentWindow != window)
+        {
+            currentWindow.Hide();
+            currentWindow = window;
+            currentWindow.Show();
+        }
     }
 }
